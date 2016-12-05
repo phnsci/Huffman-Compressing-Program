83f4818ec8852bbfa03b8f31bf425b103a4608e0
phnsci <phn10@case.edu>
2016-12-04 22:19:27 -0500

    add bin folder

--- src/CharacterList.java
+++ src/CharacterList.java
@@ -9,7 +9,6 @@ public class CharacterList {
   public CharacterList() {
   }
   
-  
   public CharacterList(char v, int c) {
     this.value = v;
     this.count = c;
@@ -33,47 +32,6 @@ public class CharacterList {
   
   public void setCount(int count) {
     this.count = count;
-  }
-  
-  public static void main(String [] args) {
-    String str = "abbcccctttayyyzz";
-    char [] original = str.toCharArray();
-    
-    /* create a sorted character array, going through alphabet order */
-    char [] sorted = str.toCharArray();
-    Arrays.sort(sorted);
-    
-    /* an array list which stores an array of CharacterList objects */
-    List<CharacterList> list = new ArrayList<CharacterList>();
-    
-    /* create the first CharacaterList object int the array list */
-    CharacterList e = new CharacterList(sorted[0], 1);
-    list.add(e);
-    
-    CharacterList oldchar = new CharacterList();
-    
-    oldchar = e;
-    System.out.println(sorted);
-    
-    /* iterate through the new sorted character array*/
-    for (int i = 1; i < sorted.length; i++) {
-      /* if find new character */
-      if (sorted[i] != sorted[i -1]) {
-        CharacterList newchar = new CharacterList(); 
-        newchar.setChar(sorted[i]);
-        newchar.setCount(1);
-        oldchar = newchar;
-        list.add(newchar);
-      }
-      /* if the character is repeated */
-      else {
-        oldchar.increment();
-      }
-    }
-    
-    for (int i = 0; i < list.size(); i++) {
-      System.out.println("i: " + i +   "Character: " + list.get(i).getChar() + "  Occurence: " + list.get(i).getCount());
-    }
   }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.nio.file.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-	    String str = FileReader.readFile("Text.txt"));
-	  }
+  public static void main(String[] args) throws IOException {
+    String str = FileReader.readFile("Text.txt");
+    char [] original = str.toCharArray();
+    
+    /* create a sorted character array, going through alphabet order */
+    char [] sorted = str.toCharArray();
+    Arrays.sort(sorted);
+    
+    /* an array list which stores an array of CharacterList objects */
+    List<CharacterList> list = new ArrayList<CharacterList>();
+    
+    /* create the first CharacaterList object int the array list */
+    CharacterList e = new CharacterList(sorted[0], 1);
+    list.add(e);
+    
+    CharacterList oldchar = new CharacterList();
+    oldchar = e;
+
+    /* iterate through the new sorted character array*/
+    for (int i = 1; i < sorted.length; i++) {
+      /* if find new character */
+      if (sorted[i] != sorted[i -1]) {
+        CharacterList newchar = new CharacterList(); 
+        newchar.setChar(sorted[i]);
+        newchar.setCount(1);
+        oldchar = newchar;
+        list.add(newchar);
+      }
+      /* if the character is repeated */
+      else {
+        oldchar.increment();
+      }
+    }
+    
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println("i: " + i +   "Character: " + list.get(i).getChar() + "  Occurence: " + list.get(i).getCount());
+    }
+  }
 }
 
